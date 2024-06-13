2a1628d064300e1031fffa6b4e0251c542dfb3e4
Hepran Putra <hepranputra@proton.me>
2024-06-13 10:23:36 +0700

    Part1: 2.11

--- sandbox.js
+++ sandbox.js
@@ -1,5 +1,16 @@
-let a = prompt("First number?", 1);
-let b = prompt("Second number?", 2);
-
-// alert(a + b); // 12
-alert(+a + +b); // 3
+let username = prompt("Enter your username");
+const ESC = null;
+if (username == "Admin") {
+    let password = prompt("Enter your password");
+    if (password == "TheMaster") {
+        alert("Welcome!");
+    } else if (password == "" || password == ESC) {
+        alert("Canceled");
+    } else {
+        alert("Wrong password");
+    }
+} else if (username == "" || username == ESC) {
+    alert("Canceled");
+} else {
+    alert("I don't know you");
+}
