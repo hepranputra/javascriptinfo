69b235cd660233fe32035a63c73fa79f82ffdcbe
Hepran Putra <hepranputra@proton.me>
2024-06-22 11:34:19 +0700

    Part 1: 5.4

--- sandbox.js
+++ sandbox.js
@@ -1,4 +1,58 @@
-console.log(extractCurrencyValue('$120') === 120); // true
+// All below should be true if the function is correct.
+console.log(getMaxSubSum([-1, 2, 3, -9]) == 5);
+console.log(getMaxSubSum([2, -1, 2, 3, -9]) == 6);
+console.log(getMaxSubSum([-1, 2, 3, -9, 11]) == 11);
+console.log(getMaxSubSum([-2, -1, 1, 2]) == 3);
+console.log(getMaxSubSum([100, -9, 2, -3, 5]) == 100);
+console.log(getMaxSubSum([1, 2, 3]) == 6);
+
+
+// Source:
+// https://javascript.info/array#a-maximal-subarray
+function getMaxSubSum(array) {
+    let partialSum = 0;
+    let maxSum = 0;
+    
+    for (let elem of array) {
+        partialSum += elem;
+        maxSum = Math.max(maxSum, partialSum);
+        
+        if (partialSum < 0) {
+            partialSum = 0;
+        }
+    }
+    
+    return maxSum;
+}
+
+
+/*
+ * Sum numbers and stop that program when ESC, CANCEL, ENTER without number
+ * or negative number is entered.
+ *
+ * @return {number} The sum
+ */
+function sumInput() {
+    let numbers = [];
+    while (true) {
+        let n = prompt("Enter a number:");
+        
+        if (!isFinite(n) || n === "" || n === null || n < 0) {
+            console.log("--> Stop program <--")
+            break;
+        }
+        
+        numbers.push(+n);
+        
+        // console.log(numbers);
+    }
+    
+    let sum = 0;
+    for (let n of numbers) {
+        sum += n;
+    }
+    return sum;
+}
 
 /*
  * Extracts number from money amount text.
